6d162b6af0fb2ad9081e5ff02ea09657dec67476
AVuPham <avupham2003@gmail.com>
2025-09-10 23:34:07 +0700

    Fix vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/solar-share/', // IMPORTANT when deploying to GitHub Pages
-  server: { port: 5173, open: true },
-  build: { outDir: 'dist' }
+  base: '/solar-share/',
 })
