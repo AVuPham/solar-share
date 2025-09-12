186271e8426d4ae404cecc828a41fb3cb223a83d
AVuPham <avupham2003@gmail.com>
2025-09-13 02:18:38 +0700

    feat: add Layout with Navbar wrapper for all pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
+// src/Layout.tsx
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Header */}
-      <header className="bg-sky-700 text-white shadow">
-        <Navbar />
-      </header>
-
-      {/* Nội dung chính */}
-      <main className="flex-grow container mx-auto px-6 py-10">
+    <>
+      <Navbar />
+      <main className="pt-16 px-6">
         <Outlet />
       </main>
-
-      {/* Footer */}
-      <footer className="bg-gray-100 text-center py-4 border-t">
-        <p className="text-gray-600 text-sm">
-          © {new Date().getFullYear()} Solar Share. All rights reserved.
-        </p>
-      </footer>
-    </div>
-  )
+    </>
+  );
 }
