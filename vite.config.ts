16c3bf30935047ed0da24a39b66177a53934fece
AVuPham <avupham2003@gmail.com>
2025-09-13 23:03:37 +0700

    fix: correct vite.config.ts syntax and set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/solar-share/',  // ⚡ bắt buộc khi deploy GitHub Pages
-})
-
-build: {
-  rollupOptions: {
-    external: ["@tensorflow/tfjs", "react-chartjs-2", "chart.js"],
+  base: "/solar-share/",
+  build: {
+    rollupOptions: {
+      external: ["@tensorflow/tfjs", "react-chartjs-2", "chart.js"],
+    },
   },
-}
+});
